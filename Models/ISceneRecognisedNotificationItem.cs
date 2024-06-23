92ee13a7cbe19987cd990146170990ffa5052c1f
HRAshton <12210721+hrashton@users.noreply.github.com>
2024-06-23 03:31:59 +0200

    Add Publisher models

---
+++ Models/ISceneRecognisedNotificationItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Bounan.Common.Models;
+
+/// <summary>
+/// Information about scenes recognised in a video.
+/// </summary>
+public interface ISceneRecognisedNotificationItem : IVideoKey
+{
+    Scenes? Scenes { get; }
+}
---
+++ Models/Notifications/ISceneRecognisedNotification.cs
@@ -0,0 +1,14 @@
+﻿namespace Bounan.Common.Models.Notifications;
+
+/// <summary>
+/// Notification that a scene(s) has been recognised / failed to recognise.
+/// </summary>
+/// <typeparam name="T">The type of the items of the notification.</typeparam>
+public interface ISceneRecognisedNotification<T>
+    where T : ISceneRecognisedNotificationItem
+{
+    /// <summary>
+    /// Gets the items of the notification.
+    /// </summary>
+    public ICollection<T> Items { get; }
+}
