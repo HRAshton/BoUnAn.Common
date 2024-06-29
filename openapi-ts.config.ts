d93cba83340ab0b2fda2427e9a0e333e190315f7
HRAshton <12210721+hrashton@users.noreply.github.com>
2024-06-30 00:17:35 +0200

    Add openapi and TS types

---
+++ openapi-ts.config.ts
@@ -0,0 +1,14 @@
+﻿import { UserConfig } from '@hey-api/openapi-ts';
+
+export default {
+    input: 'common.yaml',
+    output: {
+        path: 'ts-generated',
+        format: 'prettier',
+        lint: 'eslint',
+    },
+    schemas: false,
+    exportCore: false,
+    services: false,
+    types: true,
+} as UserConfig;
