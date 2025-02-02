de9cce4dbbd39e76ac6e558c60b65c4b011b0ca3
HRAshton <12210721+hrashton@users.noreply.github.com>
2025-02-02 12:08:18 +0100

    Add shared CDK and Runtime helpers

---
+++ ts/cdk/export-names.ts
@@ -0,0 +1,16 @@
+export enum ExportNames {
+    AlertEmail = 'alert-email',
+    LoanApiToken = 'loan-api-token',
+
+    VideoRegisteredSnsTopicArn = 'video-registered-sns-topic-arn',
+    VideoDownloadedSnsTopicArn = 'video-downloaded-sns-topic-arn',
+    SceneRecognisedSnsTopicArn = 'scene-recognised-sns-topic-arn',
+
+    GetAnimeFunctionName = 'get-anime',
+    GetVideoToDownloadFunctionName = 'get-video-to-download',
+    UpdateVideoStatusFunctionName = 'update-video-status',
+    GetSeriesToMatchFunctionName = 'get-series-to-match',
+    UpdateVideoScenesFunctionName = 'update-video-scenes',
+    UpdatePublishingDetailsFunctionName = 'update-publishing-details',
+    RegisterVideosFunctionName = 'register-videos',
+}
