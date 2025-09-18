dc8048eb5ef231d71c21fd7203dc5e27f49c2b4f
HRAshton <12210721+hrashton@users.noreply.github.com>
2025-09-18 22:00:30 +0200

    Move LoanApi endpoints to main enum

--- ts/cdk/export-names.ts
+++ ts/cdk/export-names.ts
@@ -2,6 +2,9 @@ export enum ExportNames {
   AlertEmail = 'alert-email',
   LoanApiToken = 'loan-api-token',
 
+  GetEpisodesFunctionName = 'loan-api-get-episodes-function-name',
+  GetDubsFunctionName = 'loan-api-get-dubs-function-name',
+
   VideoRegisteredSnsTopicArn = 'video-registered-sns-topic-arn',
   VideoDownloadedSnsTopicArn = 'video-downloaded-sns-topic-arn',
   SceneRecognisedSnsTopicArn = 'scene-recognised-sns-topic-arn',
@@ -13,9 +16,4 @@ export enum ExportNames {
   UpdateVideoScenesFunctionName = 'update-video-scenes',
   UpdatePublishingDetailsFunctionName = 'update-publishing-details',
   RegisterVideosFunctionName = 'register-videos',
-}
-
-export enum LoanApiExportNames {
-  GetEpisodesFunctionName = 'loan-api-get-episodes-function-name',
-  GetDubsFunctionName = 'loan-api-get-dubs-function-name',
 }
