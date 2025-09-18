34bae35029fe94fba99268e134408e34ced238f4
HRAshton <12210721+hrashton@users.noreply.github.com>
2025-09-18 21:22:18 +0200

    Add AWS loan api endpoints

--- ts/cdk/export-names.ts
+++ ts/cdk/export-names.ts
@@ -1,16 +1,21 @@
 export enum ExportNames {
-    AlertEmail = 'alert-email',
-    LoanApiToken = 'loan-api-token',
+  AlertEmail = 'alert-email',
+  LoanApiToken = 'loan-api-token',
 
-    VideoRegisteredSnsTopicArn = 'video-registered-sns-topic-arn',
-    VideoDownloadedSnsTopicArn = 'video-downloaded-sns-topic-arn',
-    SceneRecognisedSnsTopicArn = 'scene-recognised-sns-topic-arn',
+  VideoRegisteredSnsTopicArn = 'video-registered-sns-topic-arn',
+  VideoDownloadedSnsTopicArn = 'video-downloaded-sns-topic-arn',
+  SceneRecognisedSnsTopicArn = 'scene-recognised-sns-topic-arn',
 
-    GetAnimeFunctionName = 'get-anime',
-    GetVideoToDownloadFunctionName = 'get-video-to-download',
-    UpdateVideoStatusFunctionName = 'update-video-status',
-    GetSeriesToMatchFunctionName = 'get-series-to-match',
-    UpdateVideoScenesFunctionName = 'update-video-scenes',
-    UpdatePublishingDetailsFunctionName = 'update-publishing-details',
-    RegisterVideosFunctionName = 'register-videos',
+  GetAnimeFunctionName = 'get-anime',
+  GetVideoToDownloadFunctionName = 'get-video-to-download',
+  UpdateVideoStatusFunctionName = 'update-video-status',
+  GetSeriesToMatchFunctionName = 'get-series-to-match',
+  UpdateVideoScenesFunctionName = 'update-video-scenes',
+  UpdatePublishingDetailsFunctionName = 'update-publishing-details',
+  RegisterVideosFunctionName = 'register-videos',
+}
+
+export enum LoanApiExportNames {
+  GetEpisodesFunctionName = 'loan-api-get-episodes-function-name',
+  GetDubsFunctionName = 'loan-api-get-dubs-function-name',
 }
