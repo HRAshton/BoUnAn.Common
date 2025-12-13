80198180bfa8ea64bf0e4ae8a883c190223db889
HRAshton <12210721+hrashton@users.noreply.github.com>
2025-12-13 17:16:14 +0100

    Revert export-names.ts

--- ts/cdk/export-names.ts
+++ ts/cdk/export-names.ts
@@ -1,18 +1,16 @@
 export enum ExportNames {
-  AlertEmail = 'alert-email',
+    AlertEmail = 'alert-email',
+    LoanApiToken = 'loan-api-token',
 
-  LoanApiGetDubsFunctionName = 'loan-api-get-episodes-function-name',
-  LoanApiGetEpisodesFunctionName = 'loan-api-get-dubs-function-name',
+    VideoRegisteredSnsTopicArn = 'video-registered-sns-topic-arn',
+    VideoDownloadedSnsTopicArn = 'video-downloaded-sns-topic-arn',
+    SceneRecognisedSnsTopicArn = 'scene-recognised-sns-topic-arn',
 
-  VideoRegisteredSnsTopicArn = 'video-registered-sns-topic-arn',
-  VideoDownloadedSnsTopicArn = 'video-downloaded-sns-topic-arn',
-  SceneRecognisedSnsTopicArn = 'scene-recognised-sns-topic-arn',
-
-  GetAnimeFunctionName = 'get-anime',
-  GetVideoToDownloadFunctionName = 'get-video-to-download',
-  UpdateVideoStatusFunctionName = 'update-video-status',
-  GetSeriesToMatchFunctionName = 'get-series-to-match',
-  UpdateVideoScenesFunctionName = 'update-video-scenes',
-  UpdatePublishingDetailsFunctionName = 'update-publishing-details',
-  RegisterVideosFunctionName = 'register-videos',
+    GetAnimeFunctionName = 'get-anime',
+    GetVideoToDownloadFunctionName = 'get-video-to-download',
+    UpdateVideoStatusFunctionName = 'update-video-status',
+    GetSeriesToMatchFunctionName = 'get-series-to-match',
+    UpdateVideoScenesFunctionName = 'update-video-scenes',
+    UpdatePublishingDetailsFunctionName = 'update-publishing-details',
+    RegisterVideosFunctionName = 'register-videos',
 }
